51ce65bc83ddd90db897a315399fe5a9a1c34c28
Colin Billingsley <129898391+colinbillingsley@users.noreply.github.com>
2023-09-22 15:36:30 -0400

    added placeholder text for now in profile form

--- profile.php
+++ profile.php
@@ -4,25 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/styles.css">
     <title>Profile - Pokemon Manager</title>
 </head>
 
 <body>
     <header>
-        <a href="main.php" class="main-logo">POKEMON MANAGER</a>
-        <nav class="main-nav-container">
-            <ul class="main-nav-links">
-                <li>
-                    <a href="profile.php" class="main-link">Profile</a>
-                </li>
-                <li>
-                    <a href="contacts.php" class="main-link">Contacts</a>
-                </li>
-            </ul>
-            <form action="https://httpbin.org/post" method="post">
-                <button id="sign-out" class="main-button" type="submit">Sign Out</button>
-            </form>
+        <nav class="top-nav-bar">
+            <div class="left-links">
+                <a href="main.php" class="top-nav-bar-logo">POKEMON MANAGER</a>
+            </div>
+
+            <div class="right-links">
+                <ul class="top-nav-main-links">
+                    <li>
+                        <a href="profile.php" class="main-link border">Profile</a>
+                    </li>
+                    <li>
+                        <a href="contacts.php" class="main-link border">Contacts</a>
+                    </li>
+                </ul>
+
+                <button id="sign-out" class="main-button border" type="submit">Sign Out</button>
+            </div>
         </nav>
     </header>
 
@@ -32,23 +36,23 @@
                 <legend>Profile</legend>
 
                 <label for="first-name" class="f-name">First Name
-                    <input type="text" name="first-name" id="first-name" readonly>
+                    <input type="text" name="first-name" id="first-name" placeholder="First Name" readonly>
                 </label>
 
                 <label for="last-name" class="l-name">Last Name
-                    <input type="text" name="last-name" id="last-name" readonly>
+                    <input type="text" name="last-name" id="last-name" placeholder="Last Name" readonly>
                 </label>
 
                 <label for="email" class="email">Email
-                    <input type="text" name="email" id="email" readonly>
+                    <input type="text" name="email" id="email" placeholder="Email" readonly>
                 </label>
 
                 <label for="phone" class="phone-num">Phone Number
-                    <input type="tel" name="phone" id="phone" readonly>
+                    <input type="tel" name="phone" id="phone" placeholder="Phone number" readonly>
                 </label>
 
                 <label for="username" class="uname">Username
-                    <input type="text" name="username" id="username" readonly>
+                    <input type="text" name="username" id="username" placeholder="Username" readonly>
                 </label>
 
                 <label for="password" class="pass">Password
